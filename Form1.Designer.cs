f5524f88e5e70eb78bc0db4fc8e503814a375549
SaritaMT <43161374+SaritaMT@users.noreply.github.com>
2020-08-10 14:19:59 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ServerApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStopServer = new System.Windows.Forms.Button();
+            this.labelServerState = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxClients = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBoxClientData = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxHTTPData = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonStopServer
+            // 
+            this.buttonStopServer.Location = new System.Drawing.Point(56, 26);
+            this.buttonStopServer.Name = "buttonStopServer";
+            this.buttonStopServer.Size = new System.Drawing.Size(75, 23);
+            this.buttonStopServer.TabIndex = 1;
+            this.buttonStopServer.Text = "Stop Server";
+            this.buttonStopServer.UseVisualStyleBackColor = true;
+            this.buttonStopServer.Click += new System.EventHandler(this.buttonStopServer_Click);
+            // 
+            // labelServerState
+            // 
+            this.labelServerState.AutoSize = true;
+            this.labelServerState.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelServerState.Location = new System.Drawing.Point(289, 9);
+            this.labelServerState.Name = "labelServerState";
+            this.labelServerState.Size = new System.Drawing.Size(114, 15);
+            this.labelServerState.TabIndex = 5;
+            this.labelServerState.Text = "Server Not Running";
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(577, 26);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 6;
+            this.buttonExit.Text = "Close";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Clients Registered and Connection Satus";
+            // 
+            // listBoxClients
+            // 
+            this.listBoxClients.FormattingEnabled = true;
+            this.listBoxClients.Location = new System.Drawing.Point(56, 82);
+            this.listBoxClients.Name = "listBoxClients";
+            this.listBoxClients.Size = new System.Drawing.Size(596, 95);
+            this.listBoxClients.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Data Received From Client";
+            // 
+            // listBoxClientData
+            // 
+            this.listBoxClientData.FormattingEnabled = true;
+            this.listBoxClientData.Location = new System.Drawing.Point(56, 209);
+            this.listBoxClientData.Name = "listBoxClientData";
+            this.listBoxClientData.Size = new System.Drawing.Size(596, 95);
+            this.listBoxClientData.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 327);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "HTTP Data From Client";
+            // 
+            // textBoxHTTPData
+            // 
+            this.textBoxHTTPData.Location = new System.Drawing.Point(59, 353);
+            this.textBoxHTTPData.Multiline = true;
+            this.textBoxHTTPData.Name = "textBoxHTTPData";
+            this.textBoxHTTPData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxHTTPData.Size = new System.Drawing.Size(593, 85);
+            this.textBoxHTTPData.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 450);
+            this.Controls.Add(this.textBoxHTTPData);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBoxClientData);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBoxClients);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.labelServerState);
+            this.Controls.Add(this.buttonStopServer);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonStopServer;
+        private System.Windows.Forms.Label labelServerState;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxClients;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBoxClientData;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxHTTPData;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,226 @@
+﻿//SARITA TIGADI
+//1001634958
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace ServerApplication
+{
+    public partial class Form1 : Form
+    {
+        static Int32 port = 10;
+        static IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+        static TcpListener tcpListener = new TcpListener(localAddr, port);
+        static Dictionary<string, TcpClient> clientDict = new Dictionary<string, TcpClient>();
+        static List<TcpClient> clientList = new List<TcpClient>();
+        static int counter = 0;
+        public static string Message;
+        public static string ClientStatus;       
+        static TcpClient socketForClient;
+        public static int clientCount = 10;
+        public static string clientName;
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartServer();
+        }
+        /// <summary>
+        ///Reference https://stackoverflow.com/questions/11181502/c-sharp-how-to-connect-multiple-clients-to-a-tcplistener
+        ///Reference http://csharp.net-informations.com/communications/csharp-multi-threaded-server-socket.html
+        ///Reference https://stackoverflow.com/questions/15878265/single-server-multiple-clients-in-c-sharp
+        ///
+        ///The StartServer() function initializes each of the thread with the Listener function and starts the thread.
+        ///Creates an instance of TcpClient for each client that connects.
+        ///Also starts the TcpListener which is already initilized for the local host and port 10 
+        /// </summary>
+        private void StartServer()
+        {
+            labelServerState.Text = "Server Started";           
+            tcpListener.Start();
+            socketForClient = new TcpClient();
+            int numberOfClientsYouNeedToConnect = clientCount;
+            for (int i = 0; i < numberOfClientsYouNeedToConnect; i++)
+            {
+                Thread newThread = new Thread(new ThreadStart(Listeners));
+                newThread.Start();
+            }
+        }
+
+        /// <summary>
+        ///The Listeners() function accepts the connection request from the client.
+        ///Initializes the networkStream to that specific client.
+        ///It provides the client Registration and adds each client to the dictionary clientDict.
+        ///It reads the inputadata from client and based on the option selected transmits data to either that specific client or all the clients.
+        /// </summary>
+        private void Listeners()
+        {
+            /// Reference https://stackoverflow.com/questions/11181502/c-sharp-how-to-connect-multiple-clients-to-a-tcplistener
+
+            socketForClient = tcpListener.AcceptTcpClient();
+            NetworkStream networkStream = socketForClient.GetStream();
+            if (socketForClient.Connected)
+            {
+                counter++;
+                byte[] clientMessage = new byte[1024];
+                string clientData;
+                int numberOfBytesRead = 0;               
+                do
+                {
+                    numberOfBytesRead = networkStream.Read(clientMessage, 0, clientMessage.Length);
+                    clientData = Encoding.ASCII.GetString(clientMessage, 0, numberOfBytesRead);
+                }
+                while (networkStream.DataAvailable);
+              
+                textBoxHTTPData.Text = clientData.ToString();
+                string[] clientDataArray = clientData.Split('\n');
+                JObject json = JObject.Parse(clientDataArray[5]);   
+                
+                if (json.Count == 1)
+                {
+                    ///Reference:  https://stackoverflow.com/questions/12402085/how-to-parse-json-object-in-c
+                    ///Reference: https://social.msdn.microsoft.com/Forums/en-US/c0edfcc8-f889-4e04-bf88-6a0b81e8eae8/parsing-json-object?forum=csharpgeneral
+                    
+                    clientName = json["ClientName"].ToString();
+                    listBoxClients.Items.Add(clientName.ToString() + "     Connected");   
+                    clientDict.Add(clientName, socketForClient);                   
+                }
+                else if (json.Count > 1)
+                {
+                    Message = json["textMessage"].ToString();
+                    if (Message != "Disconnect")
+                    {
+                        string option = json["option"].ToString();
+                        string destClient = json["destinationClient"].ToString();
+                        listBoxClientData.Items.Add(Message.ToString());                      
+                        if (option == "option1")
+                        {
+                            sendToClient(destClient);
+                        }
+                        else if (option == "option2")
+                        {
+                            sendToAllClients();
+                        }
+                    }
+                    else if (Message == "Disconnect")
+                    {
+                        listBoxClients.Items.Add(clientName.ToString() + "     Disconnected");
+                    }              
+                                      
+                }              
+            }
+            else
+            {
+                listBoxClients.Items.Add(Message.ToString() + "     DisConnected");              
+            }                     
+        }
+
+        /// <summary>
+        ///The sendToClient(client) sends data to a specific client which is passed as a parameter
+        /// </summary>
+        static void sendToClient(string client)
+        {
+            if (clientDict.ContainsKey(client))
+            {
+                TcpClient socketForClient = clientDict[client];               
+                NetworkStream networkStream = socketForClient.GetStream();
+                /// Reference https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
+                
+                string sendResponse = Message.ToString();
+                HttpResponseMessage.HttpBody = sendResponse;
+                HttpResponseMessage.HttpContentLength = "Content - Length:" + sendResponse.Length.ToString() + "\r\n";
+                byte[] responseStream = System.Text.Encoding.ASCII.GetBytes(HttpResponseMessage.HttpHost 
+                  + HttpResponseMessage.HttpContentType + HttpResponseMessage.HttpContentLength + HttpResponseMessage.HttpBody);               
+                networkStream.Write(responseStream, 0, responseStream.Length);
+                networkStream.Flush();             
+            }
+        }
+
+        /// <summary>
+        ///The sendToAllClients() sends data to all the clients that have been registered to the server.
+        /// </summary>
+        void sendToAllClients()
+        {         
+            foreach (KeyValuePair<string, TcpClient> entry in clientDict)
+            {
+                sendToClient(entry.Key);
+                int milliseconds = 2000;
+                Thread.Sleep(milliseconds);
+            }                
+        }
+
+        /// <summary>
+        ///The buttonStopServer_Click() method is to stop the server process.
+        /// </summary>
+        private void buttonStopServer_Click(object sender, EventArgs e)
+        {
+            if (socketForClient!=null)
+            {
+                if (socketForClient.Connected)
+                {
+                    socketForClient.Close();
+                }                
+            }
+           
+            foreach (System.Diagnostics.Process myProc in System.Diagnostics.Process.GetProcesses())
+            {
+                if (myProc.ProcessName == "ServerApplication")
+                {
+                    myProc.Kill();
+                }
+            }
+            labelServerState.Text = "Server Stopped";
+        }
+
+        /// <summary>
+        ///The buttonExit_Click() method is to close the server window.
+        /// </summary>
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            foreach (System.Diagnostics.Process myProc in System.Diagnostics.Process.GetProcesses())
+            {
+                if (myProc.ProcessName == "ServerApplication")
+                {
+                    myProc.Kill();
+                }
+            }
+        }
+
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            foreach (System.Diagnostics.Process myProc in System.Diagnostics.Process.GetProcesses())
+            {
+                if (myProc.ProcessName == "ServerApplication")
+                {
+                    myProc.Kill();
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    ///The HttpResponseMessage class has the HTTP fields and formats defined that will be used by the server while sending data.
+    /// </summary>
+    /// 
+    public static class HttpResponseMessage
+    {
+        public static string HttpHost = "HTTP/1.1 200 OK\r\n";       
+        public static string HttpContentType = "Content-Type: text/json\r\n";
+        public static string HttpContentLength = "";       
+        public static string HttpBody = "";
+        public static string HttpDate = "Date: {Date}\r\n";
+    }
+}
